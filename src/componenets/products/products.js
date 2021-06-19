52fea186833baf6dcf0171a76f86348843a66f19
Syedmha <syedabedi12@gmail.com>
2021-06-19 13:01:28 +0530

    Style - Product Grid

--- src/componenets/products/products.js
+++ src/componenets/products/products.js
@@ -1,9 +1,23 @@
 import './../products/products.css';
+import Data from '../../data.json';
 
 function Products() {
     return (
       <div className="products-container">
         <h1>Product</h1>
+        <div className="grid-container">
+            {
+              Data.map(post => {
+                return (
+                  <div key={ post.id } className="grid-item"> 
+                  
+                    <h2>{ post.image }</h2>
+                    <p> { post.price }</p>
+                  </div>
+                )
+              })
+            }
+</div>
       </div>
     );
   }
